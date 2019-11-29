fa733bb0a80f73cd6089b2a2cc8792dcaf0327e8
Abubakr Abduvakhidov <51879851+Abdvkh@users.noreply.github.com>
2019-11-29 11:16:02 +0500

    Create armstrongNumber.c

---
+++ armstrongNumber.c
@@ -0,0 +1,23 @@
+//Program which checks an armstrong number
+
+#include <stdio.h>
+
+int number, digits;
+
+int countDigits(int number);
+
+int main(){
+    
+    printf("Please input the number whose armstrong will be checked: \n>");
+    scanf("%d", &number);
+    
+    digits = countDigits(number)
+}
+
+int countDigits(int number){
+    if(number > 0){
+        
+    }
+}
+
+//that's all
