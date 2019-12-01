8be565d5a1a853faf97bc96e03fea589dec7bc2a
Abubakr Abduvakhidov <51879851+Abdvkh@users.noreply.github.com>
2019-12-01 12:03:15 +0500

    Completed

--- armstrongNumber.c
+++ armstrongNumber.c
@@ -2,22 +2,27 @@
 
 #include <stdio.h>
 
-int number, digits;
+int number, digits[15], count;
 
 int countDigits(int number);
+
+int checksArm(int number, int count);
 
 int main(){
     
     printf("Please input the number whose armstrong will be checked: \n>");
     scanf("%d", &number);
     
-    digits = countDigits(number);
+    count = countDigits(number);
+    
+    checksArm(number, count);
 }
 
 int countDigits(int number){
-    int count;
     
     while(number!=0){
+        digits[count] = number%10;
+
         count++;
         
         number /= 10;
@@ -26,6 +31,34 @@ int countDigits(int number){
     printf("Total digits of the number is %d", count);
     
     return count;
+}
+
+int checksArm(int number, int count){
+    int i, sumOfTheNumberPower = 0;
+    int power = count;
+
+    for(i=0; i<count; i++){
+        int result = digits[i];
+
+        while(power!=1){
+            
+            result *= digits[i];
+            printf("The number from the digits is %d, power is %d, and the result is %d\n",digits[i], power, result);//for debugging
+            power--;
+        }
+        
+        power = count;
+        
+        sumOfTheNumberPower += result;
+    }
+    
+    printf("%d", sumOfTheNumberPower);
+    
+    if(number == sumOfTheNumberPower){
+        printf("This number is an armtrong one. Congrats you found one!");
+    }
+    
+    return 0;
 }
 
 //that's all
