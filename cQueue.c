23953230d1a7b6e000d050095de754f4b0f057a6
Abubakr Abduvakhidov <51879851+Abdvkh@users.noreply.github.com>
2020-03-10 12:39:58 +0500

    Completed version of circular queue usage

--- cQueue.c
+++ cQueue.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 #define MAX 100
 
-int n; //number of elements
+int n = 0; //number of elements
 int front = -1; //front element index
-int rare = -1; //end element index
+int end = -1; //end element index
 int size; //size of cqueue
 int cqueue[MAX]; //array aka cqueue on which operations going to be done
 int operation; //operation's code
 
+bool isFull();
+bool isEmpty();
+void printProperties(bool is_empty, bool is_full);
 void display(); // displays an array
 void enQueue(); //adds to queue given element
 void deQueue(); //pushes given element into an array
-void isEmpty(); //returns whether array is empty or not
 void top(); //returns the latest
 
 int main()
@@ -21,10 +25,10 @@ int main()
     
     printf("\nAvaliable operations on circular queue with its code:\
     \n\n1 - display elements\
-    \n2 - enter the given element to queue\
-    \n3 - take out last element from queue\
-    \n4 - return whether array is empty or not\
-    \n5 - top of stack\
+    \n2 - enter the given element to c. queue\
+    \n3 - take out 1st element from c. queue\
+    \n4 - return whether c. queue is empty/full/not\
+    \n5 - top of circular queue\
     \n6 - quit the program");
     
     do {
@@ -35,7 +39,7 @@ int main()
             case 1: display(); break;
             case 2: enQueue(); break;
             case 3: deQueue(); break;
-            case 4: isEmpty(); break;
+            case 4: printProperties(isEmpty(), isFull()); break;
             case 5: top(); break;
             default: printf("\nThere is no such command");
         }
@@ -44,48 +48,69 @@ int main()
     return 0;
 }
 
-void isEmpty(){
-    if(rare == -1){
-        printf("\nStack is empty");
+bool isEmpty(){
+    if(front == -1 && end == -1){
+        return true;
     } else {
-        printf("\nStack is not empty");
+        return false;
     }
 }
+
+bool isFull(){
+    if(n == size) return true; else return false;
+}
+
+void printProperties(bool is_empty, bool is_full){
+    switch(is_full){
+        case true: printf("\nCircular queue is full."); break;
+        case false: printf("\nCircular queue is not full."); break;
+        default: printf("\nSomething went wrong.");
+    }
+    
+    switch(is_empty){
+        case true: printf("\nCircular queue is empty."); break;
+        case false: printf("\nCircular queue is not empty."); break;
+        default: printf("\nSomething went wrong.");
+    }
+}
+
 
 void display(){
-    if(end != -1){
-    for(int i=front; i<=rare; i++) printf("%d ", array[i]);
+    if(isEmpty() == false){
+    for(int i=front; i<=end; i++) printf("%d ", cqueue[i]);
     } else {
-        isEmpty();
+        printProperties(isEmpty(), isFull());
     }
 }
 
-void push(){
+void enQueue(){
     int number;
-    
-    if(end < size){
+    if(end < size && isFull() == false){
         printf("\nEnter the number which you are going to push:");
         scanf("%d", &number);
+        front = end == -1 ? 0 : front;
+        ++n;
         end = end + 1;
-        array[end] = number;
+        cqueue[end] = number;
     } else {
-        printf("\nStack is overflow");
+        printProperties(isEmpty(), isFull());
     }
 }
 
-void pop(){
-    if(end != -1){
-        printf("\n%d element is poped", array[end]);
-        end = end - 1;
+void deQueue(){
+    if(isEmpty() == false){
+        printf("\n%d element is deleted from queue", cqueue[front]);
+        --n;
+        front = front + 1;
     } else {
-        isEmpty();
+        printProperties(isEmpty(), isFull());
     }
 }
 
 void top(){
-    if(end != -1){
-        printf("\nTop element is %d", array[end]);
+    if(isEmpty() == false   ){
+        printf("\nTop element is %d", cqueue[end]);
     } else {
-        isEmpty();
+        printProperties(isEmpty(), isFull());
     }
 }
